acca16a7e59541e8cbe5123ee7138bdcb846bc6e
Agnes-99 <Agnesmabusela8@gmail.com>
2025-04-23 01:45:39 +0200

    update readme

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
-public class Main {
+public class HelloWorld {
     public static void main(String[] args) {
-        
+        // Print a greeting message
         System.out.println("Hello, World!");
+        
+        // Example of basic arithmetic
+        int a = 10;
+        int b = 5;
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Difference: " + (a - b));
+        System.out.println("Product: " + (a * b));
+        System.out.println("Division: " + (a / b));
     }
 }
